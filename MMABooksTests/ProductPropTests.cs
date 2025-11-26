74bb67174452cc00d5a1c0334711312fa18ef9a3
Jacqueline Rael <156032975+JackieR95@users.noreply.github.com>
2025-11-25 21:50:02 -0800

    Implemented a test for ProductProps, Created a test for these methods: GetState, SetState, and Clone.
    Did not pass tests as i have yet to create the Product classes for ProductDB and Product.

---
+++ MMABooksTests/ProductPropTests.cs
@@ -0,0 +1,51 @@
+﻿using MMABooksProps;
+using NUnit.Framework;
+
+namespace MMABooksTests;
+
+[TestFixture]
+public class ProductPropsTests
+{
+    /*
+    ProductProps props;
+    [SetUp]
+    public void Setup()
+    {
+        props = new ProductProps();
+        props.ProductID = 1;
+        props.ProductCode = "N7BL5";
+        props.Description = "JavaScript and JQuery: Interactive Front-End Web Development";
+        props.UnitPrice = 35.5000m;
+        props.OnHandQuantity = 1378;
+    }
+
+    [Test]
+    public void TestGetState()
+    {
+        string jsonString = props.GetState();
+        Console.WriteLine(jsonString);
+        Assert.IsTrue(jsonString.Contains(props.ProductCode));
+        Assert.IsTrue(jsonString.Contains(props.Description));
+    }
+
+    [Test]
+    public void TestSetState()
+    {
+        string jsonString = props.GetState();
+        ProductProps newProps = new ProductProps();
+        newProps.SetState(jsonString);
+        Assert.AreEqual(props.ProductID, newProps.ProductID);
+        Assert.AreEqual(props.Description, newProps.Description);
+        Assert.AreEqual(props.ConcurrencyID, newProps.ConcurrencyID);
+    }
+
+    [Test]
+    public void TestClone()
+    {
+        CustomerProps newProps = (CustomerProps)props.Clone();
+        Assert.AreEqual(props.ProductID, newProps.ProductID);
+        Assert.AreEqual(props.Description, newProps.Description);
+        Assert.AreEqual(props.ConcurrencyID, newProps.ConcurrencyID);
+    }
+    */
+}
