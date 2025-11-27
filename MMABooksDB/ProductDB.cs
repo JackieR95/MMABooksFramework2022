432d651094751fcf07e317d8fc4f9a90166c889d
Jacqueline Rael <156032975+JackieR95@users.noreply.github.com>
2025-11-26 23:41:20 -0800

    Implemented Create method for ProductDB class

--- MMABooksDB/ProductDB.cs
+++ MMABooksDB/ProductDB.cs
@@ -21,9 +21,52 @@ namespace MMABooksDB;
 
 public class ProductDB : DBBase, IReadDB, IWriteDB
 {
-    public IBaseProps Create(IBaseProps props)
+    public IBaseProps Create(IBaseProps p)
     {
-        throw new NotImplementedException();
+
+        int rowsAffected = 0;
+        ProductProps props = (ProductProps)p;
+
+        DBCommand command = new DBCommand();
+        command.CommandText = "usp_ProductCreate";
+        command.CommandType = CommandType.StoredProcedure;
+        command.Parameters.Add("prodId", DBDbType.Int32);
+        command.Parameters.Add("prodCode", DBDbType.VarChar);
+        command.Parameters.Add("Description_p", DBDbType.VarChar);
+        command.Parameters.Add("UnitPrice_p", DBDbType.Decimal);
+        command.Parameters.Add("OnHandQuantity_p", DBDbType.Int32);
+
+
+        command.Parameters[0].Direction = ParameterDirection.Output;
+        command.Parameters["prodCode"].Value = props.ProductCode;
+        command.Parameters["Description_p"].Value = props.Description;
+        command.Parameters["UnitPrice_p"].Value = props.UnitPrice;
+        command.Parameters["OnHandQuantity_p"].Value = props.OnHandQuantity;
+
+
+
+        try
+        {
+            rowsAffected = RunNonQueryProcedure(command);
+            if (rowsAffected == 1)
+            {
+                props.ProductID = (int)command.Parameters[0].Value;
+                props.ConcurrencyID = 1;
+                return props;
+            }
+            else
+                throw new Exception("Unable to insert record. " + props.ToString());
+        }
+        catch (Exception e)
+        {
+            // log this error
+            throw;
+        }
+        finally
+        {
+            if (mConnection.State == ConnectionState.Open)
+                mConnection.Close();
+        }
     }
 
     public bool Delete(IBaseProps props)
