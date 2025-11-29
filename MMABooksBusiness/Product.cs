3707e3f78d72e4109907d1fc94fba7030837a985
Jacqueline Rael <156032975+JackieR95@users.noreply.github.com>
2025-11-29 15:56:12 -0800

    Commit: Implemented Product business class
    - Added property validation with custom getters/setters
    
    - Outlined abstract inherited methods (to be implemented later)
    
    - Set up constructors for flexible initialization

---
+++ MMABooksBusiness/Product.cs
@@ -0,0 +1,175 @@
+﻿using System;
+
+using MMABooksTools;
+using MMABooksProps;
+using MMABooksDB;
+
+using System.Collections.Generic;
+
+namespace MMABooksBusiness
+{
+    internal class Product : BaseBusiness
+    {
+        public int ProductID
+        {
+            get
+            {
+                return ((ProductProps)mProps).ProductID;
+            }
+        }
+
+        public String ProductCode
+        {
+            get
+            {
+                return ((ProductProps)mProps).ProductCode;
+            }
+
+            set
+            {
+                if (!(value == ((ProductProps)mProps).ProductCode))
+                {
+                    if (value.Trim().Length >= 1 && value.Trim().Length <= 10)
+                    {
+                        mRules.RuleBroken("ProductCode", false);
+                        ((ProductProps)mProps).ProductCode = value;
+                        mIsDirty = true;
+                    }
+
+                    else
+                    {
+                        throw new ArgumentOutOfRangeException("ProductCode must be no more than 10 characters long.");
+                    }
+                }
+            }
+        }
+
+        public String Description
+        {
+            get
+            {
+                return ((ProductProps)mProps).Description;
+            }
+
+            set
+            {
+                if (!(value == ((ProductProps)mProps).Description))
+                {
+                    if (value.Trim().Length >= 1 && value.Trim().Length <= 50)
+                    {
+                        mRules.RuleBroken("Description", false);
+                        ((ProductProps)mProps).Description = value;
+                        mIsDirty = true;
+                    }
+
+                    else
+                    {
+                        throw new ArgumentOutOfRangeException("Description must be no more than 50 characters long.");
+                    }
+                }
+            }
+        }
+
+        public decimal UnitPrice
+        {
+            get
+            {
+                return ((ProductProps)mProps).UnitPrice;
+            }
+
+            set
+            {
+                if (!(value == ((ProductProps)mProps).UnitPrice))
+                {
+                    if (value >= 0 && value <= 9999.9999m)
+                    {
+                        if (Decimal.Round(value, 4) ==value)
+                        {
+                            mRules.RuleBroken("UnitPrice", false);
+                            ((ProductProps)mProps).UnitPrice = value;
+                            mIsDirty = true;
+                        }
+                    }
+
+                    else
+                    {
+                        throw new ArgumentOutOfRangeException("UnitPrice must be between 0 and 999999.9999 and no more than 4th decimal place.");
+                    }
+                }
+            }
+        }
+
+        public int OnHandQuantity
+        {
+            get
+            {
+                return ((ProductProps)mProps).OnHandQuantity;
+            }
+
+            set
+            {
+                if (!(value == ((ProductProps)mProps).OnHandQuantity))
+                {
+                    if (value >= 0)
+                    {
+                        mRules.RuleBroken("OnHandQuantity", false);
+                        ((ProductProps)mProps).OnHandQuantity = value;
+                        mIsDirty = true;
+                    }
+
+                    else
+                    {
+                        throw new ArgumentOutOfRangeException("OnHandQuantity must be zero or greater.");
+                    }
+                }
+            }
+        }
+
+        public override object GetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void SetDefaultProperties()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void SetRequiredRules()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void SetUp()
+        {
+            throw new NotImplementedException();
+        }
+
+        #region constructors
+        /// <summary>
+        /// Default constructor - gets the connection string - assumes a new record that is not in the database.
+        /// </summary>
+        public Product() : base()
+        {
+        }
+
+        /// <summary>
+        /// Calls methods SetUp() and Load().
+        /// Use this constructor when the object is in the database AND the connection string is in a config file
+        /// </summary>
+        /// <param name="key">ID number of a record in the database.
+        /// Sent as an arg to Load() to set values of record to properties of an 
+        /// object.</param>
+        public Product(int key)
+            : base(key)
+        {
+        }
+
+        private Product(ProductProps props)
+            : base(props)
+        {
+        }
+
+        #endregion
+    }
+}
