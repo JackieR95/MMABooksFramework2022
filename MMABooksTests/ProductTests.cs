9731cc4652cbbde60026e8eea9a72f25d680198f
Jacqueline Rael <156032975+JackieR95@users.noreply.github.com>
2025-11-29 15:59:10 -0800

    Created tester page for product business class.
    Has yet to be filled

---
+++ MMABooksTests/ProductTests.cs
@@ -0,0 +1,15 @@
+﻿namespace MMABooksTests;
+
+public class ProductTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
